3232ec094b96af23e91daa0242bd0e95bee592ed
zhydani <zhydani@gmail.com>
2020-11-16 13:10:36 -0300

    inserindo o componente botao e display, iniciando a lógica para o calculo

--- App.js
+++ App.js
@@ -1,34 +1,55 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import { SafeAreaView, StyleSheet, View, FlatList, Button } from 'react-native';
 import Botao from './src/components/Button'
+import Display from './src/components/Display';
 
-export default function App() {
+export default class App extends Component {
   
-  return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <View style={styles.buttons}>
-          <Botao label="AC" />
-          <Botao label="/" />
-          <Botao label="7" />
-          <Botao label="8" />
-          <Botao label="9" />
-          <Botao label="*" />
-          <Botao label="4" />
-          <Botao label="5" />
-          <Botao label="6" />
-          <Botao label="-" />
-          <Botao label="1" />
-          <Botao label="2" />
-          <Botao label="3" />
-          <Botao label="+" />
-          <Botao label="0" />
-          <Botao label="." />
-          <Botao label="=" />
+  state = {
+    displayValue: '0'
+  }
+
+  addDigit = n => {
+    this.setState({
+      displayValue: n
+    })
+  } 
+
+  clearMemory = () => {
+    this.setState({displayValue: '0'})
+  }
+
+  setOperetion = operation => {
+
+  }
+  render (){
+    return (
+      // <SafeAreaView>
+        <View style={styles.container}>
+          <Display value={this.state.displayValue} />
+          <View style={styles.buttons}>
+            <Botao label="AC" triple onClick={this.clearMemory} />
+            <Botao label="/" operation onClick={this.setOperetion}/>
+            <Botao label="7" onClick={this.addDigit} />
+            <Botao label="8" onClick={this.addDigit}/>
+            <Botao label="9" onClick={this.addDigit} />
+            <Botao label="*" operation onClick={this.setOperetion} />
+            <Botao label="4" onClick={this.addDigit} />
+            <Botao label="5" onClick={this.addDigit} />
+            <Botao label="6" onClick={this.addDigit} />
+            <Botao label="-" operation onClick={this.setOperetion}/>
+            <Botao label="1" onClick={this.addDigit} />
+            <Botao label="2" onClick={this.addDigit} />
+            <Botao label="3" onClick={this.addDigit} />
+            <Botao label="+" operation onClick={this.setOperetion}/>
+            <Botao label="0" double onClick={this.addDigit} />
+            <Botao label="." onClick={this.addDigit}/>
+            <Botao label="=" operation onClick={this.setOperetion} />
+          </View>
         </View>
-      </View>
-    </SafeAreaView>
-  );
+      // </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
--- src/components/Button.js
+++ src/components/Button.js
@@ -7,9 +7,13 @@ import {
 } from 'react-native';
 
 const Botao = props => {
+    const stylesButton = [styles.button]
+    if (props.double) stylesButton.push(styles.buttonDouble)
+    if (props.triple) stylesButton.push(styles.buttonTriple)
+    if (props.operation) stylesButton.push(styles.operationButton)
     return (
-        <TouchableHighlight onPress={props.onClick}>
-            <Text style={styles.button}>
+        <TouchableHighlight onPress={() => props.onClick(props.label)}>
+            <Text style={stylesButton}>
                 {props.label}
             </Text>
         </TouchableHighlight>
@@ -19,13 +23,23 @@ const Botao = props => {
 const styles =  StyleSheet.create({
     button: {
         fontSize: 40,
-        height: Dimensions.get('window').height / 4,
+        height: Dimensions.get('window').width / 4,
         width: Dimensions.get('window').width / 4,
         padding: 20,
         backgroundColor: '#f0f0f0',
         textAlign: 'center',
         borderWidth: 1,
         borderColor: '#888'
+    },
+    operationButton: {
+        color: '#fff',
+        backgroundColor: '#fa8231'
+    },
+    buttonDouble: {
+        width: (Dimensions.get('window').width / 4) * 2
+    },
+    buttonTriple: {
+        width: (Dimensions.get('window').width / 4 ) * 3
     }
 });
 
