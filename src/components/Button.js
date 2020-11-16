cfe0d6db58d51b46e6df360f4a4245e3af62f9ab
zhydani <zhydani@gmail.com>
2020-11-16 11:56:35 -0300

    adicionando os botoes para o calculo

---
+++ src/components/Button.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    Dimensions,
+    TouchableHighlight
+} from 'react-native';
+
+const Botao = props => {
+    return (
+        <TouchableHighlight onPress={props.onClick}>
+            <Text style={styles.button}>
+                {props.label}
+            </Text>
+        </TouchableHighlight>
+    );
+};
+
+const styles =  StyleSheet.create({
+    button: {
+        fontSize: 40,
+        height: Dimensions.get('window').height / 4,
+        width: Dimensions.get('window').width / 4,
+        padding: 20,
+        backgroundColor: '#f0f0f0',
+        textAlign: 'center',
+        borderWidth: 1,
+        borderColor: '#888'
+    }
+});
+
+export default Botao;
