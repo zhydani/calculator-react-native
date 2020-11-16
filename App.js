9b1521cf8e2ec27e0e5781cff0a3c4f7bf536a20
zhydani <zhydani@gmail.com>
2020-11-16 13:37:44 -0300

    calculadora finalizada

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ const initialState = {
   displayValue: '0',
   clearDisplay: false,
   operation: null,
-  value: [0, 0],
+  values: [0, 0],
   current: 0
 }
 
@@ -27,7 +27,7 @@ export default class App extends Component {
 
     if (n !== '.'){
       const newValue = parseFloat(displayValue)
-      const values = [...this.state.value]
+      const values = [...this.state.values]
       values[this.state.current] = newValue
       this.setState({ values })
     }
@@ -38,7 +38,28 @@ export default class App extends Component {
   }
 
   setOperetion = operation => {
+    if (this.state.current === 0){
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } catch (e){
+        values[0] = this.state.values[0]
+      }
 
+      values[1] = 0
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        // se eu setar como true, sempre que digitar o "=", irá limpar o display
+        clearDisplay: !equals,
+        values
+      }) 
+
+    }
   }
   render (){
     return (
